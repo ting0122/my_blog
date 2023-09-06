f1ac621dd06ac42da66f29606e2af9e0f03be3c4
chen_kuan_ting <ting0919764920@gmail.com>
2023-09-06 20:11:47 +0800

    blog module

--- src/blog/blog.controller.ts
+++ src/blog/blog.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { BlogService } from './blog.service';
 
 @Controller('blog')
-export class BlogController {}
+export class BlogController {
+    constructor(private readonly blogService: BlogService){}
+
+    @Post()
+    create(@Body() postData: any){
+        return this.blogService.create(postData);
+    }
+
+    @Get()
+    findAll(){
+        return this.blogService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string){
+        return this.blogService.findOne(id);
+    }
+
+    @Put(':id')
+    update(@Param('id') id: string, @Body() postData: any){
+        return this.blogService.update(id, postData);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string){
+        return this.blogService.remove(id);
+    }
+}
--- src/blog/blog.service.ts
+++ src/blog/blog.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class BlogService {}
+export class BlogService {
+    private readonly blogPosts: any[]=[];
+
+    create(postData: any){
+        const newPost = {id: Date.now, ...postData};
+        this.blogPosts.push(newPost);
+        return newPost;
+    }
+
+    findAll(){
+        return this.blogPosts;
+    }
+
+    findOne(id: string){
+        return this.blogPosts.find(post => post.id === +id);
+    }
+
+    update(id: string, postData: any){
+        const postIndex = this.blogPosts.findIndex(post => post.id ===+id);
+        if(postIndex >= 0){
+            this.blogPosts[postIndex] = {...this.blogPosts[postIndex], ...postData};
+            return this.blogPosts[postIndex];
+        }
+        return null;
+    }
+
+    remove(id: string){
+        const postIndex = this.blogPosts.findIndex(post => post.id ===+id);
+        if(postIndex >= 0){
+            const deletedPost = this.blogPosts.slice(postIndex, 1);
+            return deletedPost[0];
+        }
+        return null;
+    }
+}
