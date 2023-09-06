4d05974e2d55be458eaad655a5418d888195f313
chen_kuan_ting <ting0919764920@gmail.com>
2023-09-06 18:21:13 +0800

    build modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BlogModule } from './blog/blog.module';
+import { UserModule } from './user/user.module';
+import { CommentModule } from './comment/comment.module';
+import { TagModule } from './tag/tag.module';
 
 @Module({
-  imports: [],
+  imports: [BlogModule, UserModule, CommentModule, TagModule],
   controllers: [AppController],
   providers: [AppService],
 })
