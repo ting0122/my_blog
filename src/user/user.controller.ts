8e25897cd1ae225c7749ffb3bcf07f97a4296471
chen_kuan_ting <ting0919764920@gmail.com>
2023-09-09 16:38:38 +0800

    user module

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private readonly userService: UserService){}
+
+    @Post()
+    createUser(@Body() userDate: any){
+        return this.userService.createUser(userDate);
+    }
+
+    @Get()
+    findAllUsers(){
+        return this.userService.findAllUsers();
+    }
+
+    @Get(':id')
+    findUserById(@Param('id') id: string){
+        return this.userService.findUserById(id);
+    }
+
+    @Put(':id')
+    updateUser(@Param('id') id: string,@Body() userData: any){
+        return this.userService.updateUser(id, userData);
+    }
+
+    @Delete(':id')
+    deleteUser(@Param('id') id: string){
+        return this.userService.deleteUser(id);
+    }
+
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    private readonly users: any[]=[];
+
+    createUser(userData: any){
+        const newUser = {id: Date.now(), ...userData};
+        this.users.push(newUser);
+        return newUser;
+    }
+
+    findAllUsers(){
+        return this.users;
+    }
+
+    findUserById(id: string){
+        return this.users.find(user => user.id===+id);
+    }
+
+    updateUser(id: string, userData: any){
+        const userIndex = this.users.findIndex(user => user.id===+id);
+        if(userIndex >= 0){
+            this.users[userIndex]={...this.users[userIndex], ...userData};
+            return this.users[userIndex];
+        }
+        return null;
+    }
+
+    deleteUser(id: string){
+        const userIndex = this.users.findIndex(user => user.id===+id);
+        if(userIndex >= 0){
+            const deleteUser = this.users.slice(userIndex, 1);
+            return deleteUser[0];
+        }
+        return null;
+    }
+
+
+}
