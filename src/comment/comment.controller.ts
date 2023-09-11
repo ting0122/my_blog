42123e4861763bb86b2ee7b80956607d79d31ba1
chen_kuan_ting <ting0919764920@gmail.com>
2023-09-11 15:49:20 +0800

    comment module built

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { CommentService } from './comment.service';
 
 @Controller('comment')
-export class CommentController {}
+export class CommentController {
+    constructor(private readonly commentService: CommentService){}
+
+    @Post()
+    createComment(@Body() commentData: any){
+        return this.commentService.createComment(commentData);
+    }
+
+    @Get()
+    findAllComments(){
+        return this.commentService.findAllComments();
+    }
+
+    @Get(':id')
+    findCommentById(@Param('id') id: string){
+        return this.commentService.findCommentById(id);
+    }
+
+    @Put('id')
+    updateComment(@Param('id') id: string, @Body() commentData: any){
+        return this.commentService.updateComment(id, commentData);
+    }
+
+    @Delete('id')
+    deleteComment(@Param('id') id: string){
+        return this.commentService.deleteComment(id);
+    }
+}
--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,4 +1,41 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class CommentService {}
+export class CommentService {
+    private readonly comments: any[]=[];
+
+    createComment(commentData: any){
+        const newComment = {id: Date.now(), ...commentData};
+        this.comments.push(newComment);
+        return newComment;
+    }
+
+    findAllComments(){
+        return this.comments;
+    }
+
+    findCommentById(id: string){
+        return this.comments.find(comment => comment.id === +id);
+    }
+
+    updateComment(id: string, commentData: any){
+        const commentIndex = this.comments.findIndex(comment => comment.id === +id);
+        if(commentIndex>=0){
+            this.comments[commentIndex] = {...this.comments[commentIndex], commentData};
+            return this.comments[commentIndex];
+        }
+        return null;
+    }
+
+    deleteComment(id: string){
+        const commentIndex = this.comments.findIndex(comment => comment.id === +id);
+        if(commentIndex>=0){
+            const deleteComment = this.comments.slice(commentIndex,1);
+            return this.comments[0];
+        }
+        return null;
+    }
+
+
+
+}
