cb368a79ba1155ebc9294a54146215096b944ca3
chen_kuan_ting <ting0919764920@gmail.com>
2023-09-18 13:34:19 +0800

    tag module build

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { TagService } from './tag.service';
 
 @Controller('tag')
-export class TagController {}
+export class TagController {
+    constructor(private readonly tagService: TagService) {}
+
+    @Post()
+    createTag(@Body() tagData: any){
+        return this.tagService.createTag(tagData);
+    }
+
+    @Get()
+    findAllTags(){
+        return this.tagService.findAllTags();
+    }
+
+    @Get(':id')
+    findTagById(@Param('id') id: string){
+        return this.tagService.findTagById(id);
+    }
+
+    @Put(':id')
+    updateTag(@Param('id') id: string, @Body() tagData: any){
+        return this.tagService.updateTag(id, tagData);
+    }
+
+    @Delete(':id')
+    deleteTag(@Param('id') id: string){
+        return this.tagService.deleteTag(id);
+    }
+
+}
--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TagService {}
+export class TagService {
+    private readonly tags: any[]=[];
+
+    createTag(tagData: any){
+        const newTag = { id: Date.now(), ...tagData };
+        this.tags.push(newTag);
+        return newTag;
+    }
+
+    findAllTags(){
+        return this.tags;
+    }
+
+    findTagById(id: string){
+        return this.tags.find(tag => tag.id ===+id);
+    }
+
+    updateTag(id: string, tagData: any){
+        const tagIndex = this.tags.findIndex(tag => tag.id === +id);
+        if(tagIndex>=0){
+            this.tags[tagIndex] = { ...this.tags[tagIndex], ...tagData };
+            return this.tags[tagIndex];
+        }
+        return null;
+    }
+
+    deleteTag(id: string){
+        const tagIndex = this.tags.findIndex(tag => tag.id === +id);
+        if(tagIndex>=0){
+            const deleteTag = this.tags.splice(tagIndex, 1);
+            return deleteTag[0];
+        }
+        return null;
+    }
+}
